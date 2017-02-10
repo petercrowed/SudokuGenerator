7abfedc733531c8c57fa09590cde2fdc43ed4cf4
Peter <petercrowed@gmail.com>
2017-02-10 14:52:45 +0100

    Cleaning Code

--- app.js
+++ app.js
@@ -1,36 +1,28 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-  var data = getSudoku();
-  // data = data[0].split("");
-  // data.remove(",");
-  console.log(data);
-
-    console.log(data);
-
+    var data = getSudoku();
     // Build page content.
     $('body').append($('<div>').addClass('wrapper')
         .append($('<div>').addClass('col')
-          .append($('<h1>').html('Sudoku'))
-          .append(generateSudokuGrid2())));
+            .append($('<h1>').html('Sudoku'))
+            .append(generateSudokuGrid2())));
 
     // Populate grids with data.
-    $('table[class^="sudoku"]').each(function (index, grid) {
+    $('table[class^="sudoku"]').each(function(index, grid) {
         populateGrid($(grid), data);
     });
 });
 
 function populateGrid(grid, data) {
-    grid.find('td').each(function (index, td) {
+    grid.find('td').each(function(index, td) {
         $(td).text(data[index] || '');
     });
 }
 
 
-
-
 function generateSudokuGrid2(data) {
-    return $('<table>').append(multiPush(9, function () {
-        return $('<tr>').append(multiPush(9, function () {
+    return $('<table>').append(multiPush(9, function() {
+        return $('<tr>').append(multiPush(9, function() {
             return $('<td>');
         }));
     })).addClass('sudoku2');
@@ -44,18 +36,20 @@ function multiPush(count, func, scope) {
     return arr;
 }
 
-Array.prototype.shuffle = function () {
+Array.prototype.shuffle = function() {
     var arr = this.valueOf();
     var ret = [];
     while (ret.length < arr.length) {
-       var x = arr[Math.floor(Number(Math.random() * arr.length))];
-       if (!(ret.indexOf(x) >= 0)) ret.push(x);
+        var x = arr[Math.floor(Number(Math.random() * arr.length))];
+        if (!(ret.indexOf(x) >= 0)) ret.push(x);
     }
     return ret;
 }
 
 Array.prototype.remove = function() {
-    var what, a = arguments, L = a.length, ax;
+    var what, a = arguments,
+        L = a.length,
+        ax;
     while (L && this.length) {
         what = a[--L];
         while ((ax = this.indexOf(what)) !== -1) {
@@ -64,8 +58,3 @@ Array.prototype.remove = function() {
     }
     return this;
 };
-// function getSudoku() {
-//
-//
-//   return board;
-// }
--- sudokuGenerator.js
+++ sudokuGenerator.js
@@ -1,11 +1,10 @@
-function getSudoku(){
+function getSudoku() {
 
 
-var board = new Array(9);
-for(var i = 0; i < 9; i++)
-{
-  board[i] = new Array(9);
-}
+    var board = new Array(9);
+    for (var i = 0; i < 9; i++) {
+        board[i] = new Array(9);
+    }
 
 
     function nextBoard(difficulty) {
@@ -26,8 +25,8 @@ for(var i = 0; i < 9; i++)
 
         for (var i = top - 1; i > 0; i--) {
             current = Math.floor(Math.random() * i);
-          //  console.log(i);
-          //  console.log("Current" + current);
+            //  console.log(i);
+            //  console.log("Current" + current);
             tmp = toCheck[current];
             toCheck[current] = toCheck[i];
             toCheck[i] = tmp;
@@ -51,98 +50,87 @@ for(var i = 0; i < 9; i++)
                 }
                 if (nextCell(nextX, nextY)) {
                     return true;
-                      }
-              }
+                }
             }
-
-            board[x][y] = 0;
-            return false;
         }
 
+        board[x][y] = 0;
+        return false;
+    }
 
-        function legalMove(x, y, current) {
 
-            for (var i = 0; i < 9; i++) {
+    function legalMove(x, y, current) {
 
-                if (current === board[x][i])
-                {
+        for (var i = 0; i < 9; i++) {
+
+            if (current === board[x][i]) {
+                return false;
+            }
+
+
+        }
+        for (var i = 0; i < 9; i++) {
+            if (current === board[i][y]) {
+                return false;
+            }
+        }
+        var cornerX = 0;
+        var cornerY = 0;
+        if (x > 2) {
+            if (x > 5) {
+                cornerX = 6;
+            } else {
+                cornerX = 3;
+            }
+        }
+        if (y > 2) {
+            if (y > 5) {
+                cornerY = 6;
+            } else {
+                cornerY = 3;
+            }
+        }
+
+        for (var i = cornerX; i < 10 && i < cornerX + 3; i++)
+            for (var j = cornerY; j < 10 && j < cornerY + 3; j++)
+                if (current == board[i][j])
                     return false;
-                }
 
+        return true;
+    }
+
+    function makeHoles(holesToMake) {
+        var remainingSquares = 81;
+        var remainingHoles = holesToMake;
+
+        for (var i = 0; i < 9; i++)
+            for (var j = 0; j < 9; j++) {
+                var holeChance = remainingHoles / remainingSquares;
+                if (Math.random() < holeChance) {
+                    board[i][j] = 0;
+                    remainingHoles--;
+                }
+                remainingSquares--;
+            }
+    }
+
+    var numbers = [];
+
+    function print() {
+        for (var i = 0; i < 9; i++) {
+            for (var j = 0; j < 9; j++) {
+
+                numbers.push(board[i][j]);
+                //  console.log(board[i][j]);
 
             }
-            for (var i = 0; i < 9; i++) {
-                if (current === board[i][y])
-              {
-                return false;
-              }
-            }
-            var cornerX = 0;
-            var cornerY = 0;
-            if (x > 2) {
-                if (x > 5) {
-                    cornerX = 6;
-                } else {
-                    cornerX = 3;
-                }
-            }
-            if (y > 2)
-              {
-                if(y > 5)
-                {
-                  cornerY = 6;
-                }
-                else
-                {
-                  cornerY = 3;
-                }
-              }
 
-            for(var i = cornerX; i < 10 && i < cornerX + 3; i++)
-              for(var j = cornerY; j < 10 && j < cornerY + 3; j++)
-                if(current == board[i][j])
-                return false;
-
-            return true;
         }
+    }
 
-        function makeHoles(holesToMake)
-        {
-          var remainingSquares = 81;
-          var remainingHoles = holesToMake;
-
-          for(var i = 0; i < 9 ; i++)
-            for(var j = 0; j < 9; j++)
-            {
-              var holeChance = remainingHoles/ remainingSquares;
-              if(Math.random() < holeChance)
-              {
-                board[i][j] = 0;
-                remainingHoles--;
-              }
-              remainingSquares--;
-            }
-        }
-
-var numbers = [];
-        function print()
-        {
-          for(var i = 0; i< 9; i++)
-          {
-            for (var j = 0; j < 9; j++)
-            {
-
-              numbers.push(board[i][j]);
-        //  console.log(board[i][j]);
-
-            }
-
-          }
-        }
-
-    var table = nextBoard(35);
+    var table = nextBoard(49);
 
     print();
-  //  console.log(numbers);
-  return numbers;
-  }
+    //  console.log(numbers);
+    return numbers;
+}
